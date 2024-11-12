db10905f1bdec3f6f1bdd413a13e6529ebb0544f
esteranodin <esteranodin@hotmail.fr>
2024-11-12 13:36:22 +0100

    toutes les corrections

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,3 +1,5 @@
+// en 4 if
+
 // let a = 10;
 
 // if (a % 3 === 0 && a % 5 !== 0) {
@@ -16,6 +18,29 @@
 //      console.log(a);
 //  }
 
+
+// en 3 if
+
+// let nombre = 30;
+
+// let resultat ="le resultat est ";
+
+// if (nombre % 3 === 0){
+//     resultat = resultat + "fizz";
+// }
+
+// if (nombre % 5 === 0){
+//     resultat = resultat + "buzz";
+// }
+
+// if (nombre % 5 != 0 && nombre % 3 != 0) {
+//     resultat = resultat + nombre
+// }
+
+// console.log(resultat);
+
+
+// en 2 if
 let a = 11;
 
 let resultat ="";
@@ -29,3 +54,4 @@ if (a % 5 === 0) {
 }
 
 console.log(resultat || a);
+
