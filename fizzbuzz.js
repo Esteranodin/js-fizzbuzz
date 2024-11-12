9055784b7b595a4f152d36e9c4abac33057dee02
esteranodin <esteranodin@hotmail.fr>
2024-11-12 12:33:45 +0100

    correction en 2 if

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,15 +1,31 @@
-let a = 2;
+// let a = 10;
 
-if (a % 3 === 0 && a % 5 !== 0) {
-    console.log("fizz");
-}
-else if (a % 5 === 0 && a % 3 !== 0) {
-    console.log("buzz");
-}
-else if (a % 5 === 0 && a % 3 == 0) {
-    console.log("fizzbuzz");
-}
-else if (a % 3 !== 0 && a % 5 !== 0) {
-    console.log(a);
+// if (a % 3 === 0 && a % 5 !== 0) {
+//     console.log("fizz");
+// }
+
+// if (a % 5 === 0 && a % 3 !== 0) {
+//     console.log("buzz");
+// }
+
+// if (a % 5 === 0 && a % 3 === 0) {
+//     console.log("fizzbuzz");
+// }
+
+// else if (a % 3 !== 0 && a % 5 !== 0) {
+//      console.log(a);
+//  }
+
+let a = 11;
+
+let resultat ="";
+
+if (a % 3 === 0) {
+    resultat = resultat + "fizz";
 }
 
+if (a % 5 === 0) {
+    resultat = resultat + "buzz";
+}
+
+console.log(resultat || a);
